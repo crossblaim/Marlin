b0cbd84f290c79d909b5ed71892b5cc6665d7c34
Claudio Perez Gamayo <crossblaim@gmail.com>
2014-12-01 12:59:46 +0100

    Enable QUICKHOME

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -217,7 +217,7 @@
 #define Y_HOME_RETRACT_MM 5
 #define Z_HOME_RETRACT_MM 5 // deltas need the same for all three axis
 
-//#define QUICK_HOME  //if this is defined, if both x and y are to be homed, a diagonal move will be performed initially.
+#define QUICK_HOME  //if this is defined, if both x and y are to be homed, a diagonal move will be performed initially.
 
 #define AXIS_RELATIVE_MODES {false, false, false, false}
 
